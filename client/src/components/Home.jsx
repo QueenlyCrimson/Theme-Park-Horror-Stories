38cb8408800c63e1b4bfeafda05fb688cddeb59c
Truman Miller <trumanmiller20@gmailcom>
2023-02-16 16:21:01 -0700

    complete axios call to get seeded stories

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,16 @@
-const Home = (props) => {
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+const Home = ({ stories, setStories }) => {
+  const getAllStories = async () => {
+    const res = await axios.get("http://localhost:3001/stories")
+    setStories(res.data.stories)
+  }
+
+  useEffect(() => {
+    getAllStories()
+  }, [])
+
   return <div></div>
 }
 
