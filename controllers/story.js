66e51d42825f7cf20fa62317824bce66c694dfb0
Truman Miller <trumanmiller20@gmailcom>
2023-02-16 19:34:52 -0700

    axios to get story by id, populate on page

--- controllers/story.js
+++ controllers/story.js
@@ -1,64 +1,62 @@
-const Story = require("../models/story");
+const Story = require("../models/story")
 
 const createStory = async (req, res) => {
   try {
-    const story = await new Story(req.body);
-    await story.save();
+    const story = await new Story(req.body)
+    await story.save()
     return res.status(201).json({
       story,
-    });
+    })
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message })
   }
-};
+}
 
 const getAllStories = async (req, res) => {
   try {
-    const stories = await Story.find();
-    return res.status(200).json({ stories });
+    const stories = await Story.find()
+    return res.status(200).json({ stories })
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 const getStoryById = async (req, res) => {
   try {
-    const { Name } = req.params;
-    const story = await Story.findById(Name);
+    const { id } = req.params
+    const story = await Story.findById(id)
     if (story) {
-      return res.status(200).json({ story });
+      return res.status(200).json({ story })
     }
-    return res
-      .status(404)
-      .send("Story with the specified name does not exist!");
+    return res.status(404).send("Story with the specified name does not exist!")
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 const updateStory = async (req, res) => {
   try {
     const story = await Story.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-    });
-    res.status(200).json(story);
+    })
+    res.status(200).json(story)
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 const deleteStory = async (req, res) => {
   try {
-    const { id } = req.params;
-    const deleted = await Story.findByIdAndDelete(id);
+    const { id } = req.params
+    const deleted = await Story.findByIdAndDelete(id)
     if (deleted) {
-      return res.status(200).send("Story deleted");
+      return res.status(200).send("Story deleted")
     }
-    throw new Error("Story not found");
+    throw new Error("Story not found")
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 module.exports = {
   createStory,
@@ -66,4 +64,4 @@ module.exports = {
   getStoryById,
   updateStory,
   deleteStory,
-};
+}
