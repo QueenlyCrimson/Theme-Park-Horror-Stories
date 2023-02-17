b2e5e6a06a518c3eaeda67087ce92ac3f6af5786
Truman Miller <trumanmiller20@gmailcom>
2023-02-16 18:48:42 -0700

    navigate to ViewStory comp on button click

--- client/src/components/StoryTitle.jsx
+++ client/src/components/StoryTitle.jsx
@@ -1,10 +1,21 @@
-const Story = (props) => {
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
+const StoryTitle = (props) => {
+  let navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(`/viewstory/${props.id}`)
+  }
+
   return (
     <div className="story-title">
       {props.title}
-      <button>View Story</button>
+      <button onClick={() => handleClick()} id={props.id}>
+        View Story
+      </button>
     </div>
   )
 }
 
-export default Story
+export default StoryTitle
