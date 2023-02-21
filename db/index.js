00352bbeb95a6a304d9363fbe115e7df1f7afbee
QueenlyCrimson <ccinemainn@gmail.com>
2023-02-21 13:56:34 -0600

    Adjustments to the comments and story controllers as well as the completion of db/index.js

---
+++ db/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+  })
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
+
+const db = mongoose.connection;
+
+module.exports = db;
