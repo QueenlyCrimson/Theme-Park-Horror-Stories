3462c818f8ab87d297b0b89613d205f9cb1fde88
QueenlyCrimson <ccinemainn@gmail.com>
2023-02-21 14:31:41 -0600

    Routes adjusted

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
-const { Router } = require('express')
-const router = Router()
-const storyController = require('../controllers/stories')
-const commentController = require('../controllers/comments')
+const { Router } = require("express");
+const router = Router();
+const storyController = require("../controllers/story");
+const commentController = require("../controllers/comments");
 
-router.get('/', (req, res) => res.send('This is Root!'))
+router.get("/", (req, res) => res.send("This is Root!"));
 
 // post controllers
-router.post('/stories/create', storyController.createStory)
-router.post('/comments/create/:storyId', commentController.createComment)
+router.post("/stories/create", storyController.createStory);
+router.post("/comments/create/:storyId", commentController.createComment);
 
 // get controllers
-router.get('/stories', storyController.getAllStories)
-router.get('/stories/:id', storyController.getStoryById)
-router.get('/comments', commentController.getAllComments)
-router.get('/comments/story/:storyId', commentController.getCommentsByStoryId)
+router.get("/stories", storyController.getAllStories);
+router.get("/stories/:id", storyController.getStoryById);
+router.get("/comments", commentController.getAllComments);
+router.get("/comments/story/:storyId", commentController.getCommentsByStoryId);
 
 // delete controllers
-router.delete('/stories/delete/:id', storyController.deleteStory)
-router.delete('/comments/delete/:id', commentController.deleteComment)
+router.delete("/stories/delete/:id", storyController.deleteStory);
+router.delete("/comments/delete/:id", commentController.deleteComment);
 
-module.exports = router
+module.exports = router;
