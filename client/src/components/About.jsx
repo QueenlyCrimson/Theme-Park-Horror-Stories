a8e0b8ed8542e8e29d0181daba0cc5122ba1d6c2
Jonshea Nutson <yuurieru@icloud.com>
2023-02-21 16:44:12 -0500

    added about content to the about component

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,5 +1,35 @@
 const About = (props) => {
-  return <div></div>
+    return (
+        <div>
+            <h3>About Horror Park</h3>
+            <h5>
+                Welcome to our website dedicated to theme park horror stories.
+                <br />
+                Here, we share some of the scariest and most unsettling
+                incidents that have occurred at amusement parks and water parks
+                around the world. While theme parks are designed to be fun and
+                exciting, accidents and malfunctions can and do happen.
+                <br />
+                These stories serve as a reminder that even the most
+                well-maintained rides and attractions can pose a risk to
+                visitors. Our goal is not to discourage people from going to
+                theme parks, but rather to raise awareness about the importance
+                of following safety guidelines and regulations, and to highlight
+                the need for theme parks to prioritize the safety of their
+                guests above all else. We hope that by sharing these stories, we
+                can help educate people about the potential risks associated
+                with theme parks, and encourage both visitors and theme park
+                operators to take safety seriously. We also hope that these
+                stories serve as a tribute to those who have been affected by
+                accidents and incidents at theme parks.
+                <br />
+                Please note that these stories may be unsettling or triggering
+                for some readers. We encourage everyone to exercise caution and
+                to prioritize their own safety when visiting theme parks or you
+                might be next...
+            </h5>
+        </div>
+    )
 }
 
 export default About
