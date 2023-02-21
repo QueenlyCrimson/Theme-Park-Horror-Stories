f533f32f2f3b22c5a7499c965f174f6c6cd26348
Jonshea Nutson <yuurieru@icloud.com>
2023-02-21 14:54:16 -0500

    updated controller routes & added put routes

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,25 @@
 const { Router } = require('express')
 const router = Router()
-const storyController = require('../controllers/stories')
-const commentController = require('../controllers/comments')
+const Controllers = require('../controllers/index')
 
 router.get('/', (req, res) => res.send('This is Root!'))
 
 // post controllers
-router.post('/stories/create', storyController.createStory)
-router.post('/comments/create/:storyId', commentController.createComment)
+router.post('/stories/create', Controllers.createStory)
+router.post('/comments/create/:storyId', Controllers.createComment)
 
 // get controllers
-router.get('/stories', storyController.getAllStories)
-router.get('/stories/:id', storyController.getStoryById)
-router.get('/comments', commentController.getAllComments)
-router.get('/comments/story/:storyId', commentController.getCommentsByStoryId)
+router.get('/stories', Controllers.getAllStories)
+router.get('/stories/:id', Controllers.getStoryById)
+router.get('/comments', Controllers.getAllComments)
+router.get('/comments/story/:storyId', Controllers.getCommentsByStoryId)
+
+//put controllers
+router.put('/stories/update/:id', Controllers.updateStory)
+router.put('/comments/update/:id', Controllers.updateComment)
 
 // delete controllers
-router.delete('/stories/delete/:id', storyController.deleteStory)
-router.delete('/comments/delete/:id', commentController.deleteComment)
+router.delete('/stories/delete/:id', Controllers.deleteStory)
+router.delete('/comments/delete/:id', Controllers.deleteComment)
 
 module.exports = router
