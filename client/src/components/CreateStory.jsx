8819e96e1c054a03ec9ce217a84f056d41bc4682
Jonshea Nutson <yuurieru@icloud.com>
2023-02-22 10:04:20 -0500

    added styling to create story form & update about

--- client/src/components/CreateStory.jsx
+++ client/src/components/CreateStory.jsx
@@ -1,79 +1,99 @@
-import { useState } from "react"
-import axios from "axios"
+import { useState } from 'react'
+import axios from 'axios'
+import '../styles/CreateStory.css'
 
 const CreateStory = ({ stories, setStories }) => {
-  const [newStory, setNewStory] = useState({
-    title: "",
-    image: "",
-    park: "",
-    date: "",
-    content: "",
-  })
-
-  const handleChange = (e) => {
-    setNewStory({ ...newStory, [e.target.id]: e.target.value })
-  }
-
-  const createStory = async (e) => {
-    e.preventDefault()
-    let res = await axios.post("http://localhost:3001/stories/create", newStory)
-    let currentStories = stories
-    currentStories.push(res.data.story)
-    setStories(currentStories)
-    setNewStory({
-      title: "",
-      image: "",
-      park: "",
-      date: "",
-      content: "",
+    const [newStory, setNewStory] = useState({
+        title: '',
+        image: '',
+        park: '',
+        date: '',
+        content: '',
     })
-  }
 
-  const handleSubmit = (e) => {
-    createStory(e)
-  }
+    const handleChange = (e) => {
+        setNewStory({ ...newStory, [e.target.id]: e.target.value })
+    }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="title">Title:</label>
-      <input
-        type="text"
-        id="title"
-        onChange={handleChange}
-        value={newStory.title}
-      />
-      <label htmlFor="image">Image URL:</label>
-      <input
-        type="text"
-        id="image"
-        onChange={handleChange}
-        value={newStory.image}
-      />
-      <label htmlFor="park">Park Name:</label>
-      <input
-        type="text"
-        id="park"
-        onChange={handleChange}
-        value={newStory.park}
-      />
-      <label htmlFor="date">Date of Event:</label>
-      <input
-        type="text"
-        id="date"
-        onChange={handleChange}
-        value={newStory.date}
-      />
-      <label htmlFor="content">Share Story:</label>
-      <textarea
-        id="content"
-        cols="30"
-        rows="10"
-        onChange={handleChange}
-        value={newStory.content}
-      ></textarea>
-      <button type="submit">Add Story</button>
-    </form>
-  )
+    const createStory = async (e) => {
+        e.preventDefault()
+        let res = await axios.post(
+            'http://localhost:3001/stories/create',
+            newStory
+        )
+        let currentStories = stories
+        currentStories.push(res.data.story)
+        setStories(currentStories)
+        setNewStory({
+            title: '',
+            image: '',
+            park: '',
+            date: '',
+            content: '',
+        })
+    }
+
+    const handleSubmit = (e) => {
+        createStory(e)
+    }
+
+    return (
+        <form className='createStory-container' onSubmit={handleSubmit}>
+            <label className='createStory-label' htmlFor='title'>
+                Title:
+            </label>
+            <input
+                className='createStory-input'
+                type='text'
+                id='title'
+                onChange={handleChange}
+                value={newStory.title}
+            />
+            <label className='createStory-label' htmlFor='image'>
+                Image URL:
+            </label>
+            <input
+                className='createStory-input'
+                type='file'
+                id='image'
+                onChange={handleChange}
+                value={newStory.image}
+            />
+            <label className='createStory-label' htmlFor='park'>
+                Park Name:
+            </label>
+            <input
+                className='createStory-input'
+                type='text'
+                id='park'
+                onChange={handleChange}
+                value={newStory.park}
+            />
+            <label className='createStory-label' htmlFor='date'>
+                Date of Event:
+            </label>
+            <input
+                className='createStory-input'
+                type='text'
+                id='date'
+                onChange={handleChange}
+                value={newStory.date}
+            />
+            <label className='createStory-label' htmlFor='content'>
+                Share Story:
+            </label>
+            <textarea
+                className='createStory-textarea'
+                id='content'
+                cols='30'
+                rows='10'
+                onChange={handleChange}
+                value={newStory.content}></textarea>
+            <button className='createStory-button' type='submit'>
+                Add Story
+            </button>
+        </form>
+    )
 }
 
 export default CreateStory
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,22 +1,23 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from 'react-router-dom'
+import '../styles/Navbar.css'
 
 const NavBar = (props) => {
-  return (
-    <div className="navbar">
-      <h1>HORROR PARK</h1>
-      <div className="navlinks">
-        <NavLink to="/" className="navlink">
-          HOME
-        </NavLink>
-        <NavLink to="/about" className="navlink">
-          ABOUT
-        </NavLink>
-        <NavLink to="/createstory" className="navlink">
-          ADD STORY
-        </NavLink>
-      </div>
-    </div>
-  )
+    return (
+        <div className='navbar-container'>
+            <h1 className='navbar-header'>HORROR PARK</h1>
+            <div className='navlinks-container'>
+                <NavLink to='/' className='navlink'>
+                    HOME
+                </NavLink>
+                <NavLink to='/about' className='navlink'>
+                    ABOUT
+                </NavLink>
+                <NavLink to='/createstory' className='navlink'>
+                    ADD STORY
+                </NavLink>
+            </div>
+        </div>
+    )
 }
 
 export default NavBar
