4f0f428bf2e16bd65d181496fc66be918cc17ecb
Jonshea Nutson <yuurieru@icloud.com>
2023-02-23 09:52:14 -0500

    deployed

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,37 +1,35 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
-import "../styles/Comment.css"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import '../styles/Comment.css'
 
 const Comment = (props) => {
-  const [comments, setComments] = useState([])
-  console.log(props)
-  const getComments = async () => {
-    const res = await axios.get(
-      `http://localhost:3001/comments/story/${props.id}`
-    )
-    console.log(res.data)
-    setComments(res.data.comment)
-  }
+    const [comments, setComments] = useState([])
+    console.log(props)
+    const getComments = async () => {
+        const res = await axios.get(`/comments/story/${props.id}`)
+        console.log(res.data)
+        setComments(res.data.comment)
+    }
 
-  useEffect(() => {
-    getComments()
-  }, [])
+    useEffect(() => {
+        getComments()
+    }, [])
 
-  return (
-    <div>
-      {comments.map((comment) => (
-        <div className="comment-text" key={comment._id}>
-          <p>
-            <br />
-            username: {comment.username}
-            <br />
-            {comment.content}
-            <br />
-          </p>
+    return (
+        <div>
+            {comments.map((comment) => (
+                <div className='comment-text' key={comment._id}>
+                    <p>
+                        <br />
+                        username: {comment.username}
+                        <br />
+                        {comment.content}
+                        <br />
+                    </p>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  )
+    )
 }
 
 export default Comment
